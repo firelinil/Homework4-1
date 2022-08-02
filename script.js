7f23a93c773cb79dfb0c0ec9dde491a9be606008
firelinil <109924100+firelinil@users.noreply.github.com>
2022-08-03 00:15:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let fromPx = +prompt("Напиши количество пикселей и я переведу их в em!","Например, 48");
+let prostoPxInEm = 16;
+let result = fromPx/prostoPxInEm;
+
+alert (result +" "+ "em")
